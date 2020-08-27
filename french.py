58820bb6d66d295aea3e71bc4e09ebc6c4d976ae
Gael DOUHAIZENET <douhaga@s4dad.aws.cloud.tech.ec.europa.eu>
2020-08-27 15:12:54 +0200

    original commit

---
+++ french.py
@@ -0,0 +1,54 @@
+from transformers import BertModel, BertTokenizer
+import torch
+from scipy.spatial.distance import cdist
+import spacy
+import numpy as np
+
+# prepare distances selection,
+# see also https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html
+distances = [
+    'euclidean',
+    # 'minkowski',
+    'cityblock',
+    # 'sqeuclidean',
+    'cosine',
+    # 'correlation',
+    # 'hamming',
+    # 'jaccard',
+    # 'chebyshev',
+    # 'canberra'
+]
+
+# pre-trained models loading
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+model = BertModel.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+nlp = spacy.load('fr_core_news_sm')
+
+# for example, obviously
+test_sentence = "Il est bientôt l'heure de déjeuner."
+
+# create the base embeddings
+input_sentence = torch.tensor(tokenizer.encode(test_sentence)).unsqueeze(0)
+out = model(input_sentence)
+last_layer_mbdgs = out[0]
+base_cls_mbdgs = last_layer_mbdgs[0][0]
+
+# load text
+doc = nlp("J'ai envie de déjeuner. Va-t-on bientôt déjeuner ? Parlons d'autre chose.")
+
+# iterate over the sentences
+for sent in doc.sents:
+    print(f'\n{test_sentence}\n{sent.text}')
+    comp_sentence = torch.tensor(tokenizer.encode(sent.text)).unsqueeze(0)
+    comp_last_layer_mbdgs = model(comp_sentence)[0]
+    comp_cls_mbdgs = comp_last_layer_mbdgs[0][0]
+
+    # compare embedding to the base cls token (used for classification)
+    for d_type in distances:
+        try:
+            distance = cdist(np.array([base_cls_mbdgs.detach().numpy()]),
+                             np.array([comp_cls_mbdgs.detach().numpy()]),
+                             d_type)
+            print('{} distance:\t {:0>.3f}'.format(d_type, np.squeeze(distance)))
+        except Exception as e:
+            print(str(type(e)), e.args[0])
